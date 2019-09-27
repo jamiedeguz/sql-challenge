38a0cb72ee55623cc9776242ce47dbf67d85c76e
jamiedg <jamiejdeguzman@gmail.com>
2019-09-26 18:02:00 -0700

    Adding missed solution

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -151,3 +151,11 @@ WHERE dept_name = 'Sales';
 -- employee number, last name, first name, and department name.
 SELECT * from employee_assignments
 WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+--8. In descending order, list the frequency count of employee last names, 
+-- i.e., how many employees share each last name.
+SELECT 
+	employee_assignments.last_name,
+	COUNT(employee_assignments.last_name) 
+FROM employee_assignments
+GROUP BY employee_assignments.last_name
